a8e0a409861ad704a690f3f7a5ae043bb8c2b525
Pablo Burns-Roa <pabloburnsroa@googlemail.com>
2022-10-25 14:20:46 +0100

    just starting off here

---
+++ sandbox1.ts
@@ -0,0 +1,17 @@
+/*
+TypeScript checks if types have been consitently assigned
+Highlight any unexpected behaviour in your code  i.e. lowering the chance of bugs
+
+*/
+
+// Implicit
+let helloWorld = 'Hello World';
+// Explicit
+let helloWorld2: string = 'Hello World';
+
+// Defining Types
+
+const user = {
+  name: 'Pablo',
+  id: 0,
+};
